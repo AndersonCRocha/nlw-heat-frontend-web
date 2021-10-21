aaf9c1ae1d1eac9841fb8a17d4a9e1284dac794f
Anderson Rocha <andersoncristiano@frwk.com.br>
2021-10-20 21:21:30 -0300

    feat: create message list component

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,3 +1,37 @@
+import styles from "./styles.module.scss";
+
+import logoImg from "../../assets/logo.svg";
+
 export function MessageList() {
-  return <div>MessageList</div>;
+  return (
+    <div className={styles.messageListWrapper}>
+      <img src={logoImg} alt="DoWhile 2021" />
+
+      <ul className={styles.messageList}>
+        {Array(3)
+          .fill(0)
+          .map(() => (
+            <li className={styles.message}>
+              <p className={styles.messageContent}>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book.
+              </p>
+
+              <div className={styles.messageUser}>
+                <div className={styles.userImage}>
+                  <img
+                    src="https://github.com/andersoncrocha.png"
+                    alt="Anderson Rocha"
+                  />
+                </div>
+
+                <span>Anderson Rocha</span>
+              </div>
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
 }
